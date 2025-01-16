cf67d3ed6253eda8eea99ce76945f8da3c8b1c69
emmad276 <emmadahmed90@gmail.com>
2025-01-17 04:24:54 +0500

    update day 2

---
+++ day_2/SanitySchema.js
@@ -0,0 +1,11 @@
+export default {
+    name: 'product',
+    type: 'document',
+    fields: [
+      { name: 'name', type: 'string', title: 'Product Name' },
+      { name: 'price', type: 'number', title: 'Price' },
+      { name: 'stock', type: 'number', title: 'Stock Level' },
+      { name: 'category', type: 'string', title: 'Category' },
+      { name: 'image', type: 'image', title: 'Product Image' }
+    ]
+  };
